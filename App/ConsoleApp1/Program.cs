a5eb384a2459b429cfc00ad19095af109c638f66
Diego <dhiego_ti@hotmail.com>
2022-12-12 18:47:54 -0300

    exercicios

--- App/ConsoleApp1/Program.cs
+++ App/ConsoleApp1/Program.cs
@@ -10,27 +10,6 @@ namespace ConsoleApp1
     internal class Program
     {
         static void Main(string[] args) {
-            // Calculo da média
-
-            //
-            int[] notas = { 4, 5,6,7};
-            string[,] resultadoMedia = {
-                { "5", "Reprovado" },
-                { "7", "Recuperação" },
-                { "8", "Aprovado" },
-            };
-
-            int soma = 0;
-            for (int i = 0; i < notas.Length; i++) {
-                soma += notas[i];
-            }
-            double media = soma / notas.Length;
-
-            Console.WriteLine(resultadoMedia[1, 0]);
-           
-
-            
-
         }
     }
 }
